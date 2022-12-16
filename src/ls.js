50b6d8d678840a55c26544ef3144c2b957b1e06c
Stanislau Yarotski <stas_yar@tut.by>
2022-12-17 02:50:01 +0300

    refactor: refactoring ls function

--- src/ls.js
+++ src/ls.js
@@ -1,26 +1,21 @@
-import { readdir, stat, realpath } from 'fs/promises';
-import { resolve } from "path";
+import { readdir } from 'fs/promises';
 import { ERROR_OPERATION_FAILED } from "./constants.js";
 import { WORK_PATH } from "./operations.js";
 import { greenColor, lightblueColor } from './utils/redraw.js';
 
 
-const LiST_STRING = () => lightblueColor('Directory of ') + greenColor(WORK_PATH.url);
+const LiST_STRING = (path) => lightblueColor('Directory of ') + greenColor(path);
 
 export const ls = async () => {
   try {
-    if ((await stat(WORK_PATH.url)).isDirectory()) {
-      console.log(LiST_STRING());
-      const result = await readdir(WORK_PATH.url, { withFileTypes: true });
-      const results = result.map((item) => {
-        const newItem = ({
-          Name: item.name,
-          Type: item.isFile() ? 'file' : item.isDirectory() ? 'directory' : item.isSymbolicLink() ? 'symbolic link' : undefined,
-        });
-        return newItem;
-      });
-      return results.sort((a, b) => a.Type < b.Type ? -1 : 0);
-    }
+    console.log(LiST_STRING(WORK_PATH.url));
+    const results = await readdir(WORK_PATH.url, { withFileTypes: true });
+    return results
+      .map((item) => ({
+        Name: item.name,
+        Type: item.isDirectory() ? 'directory' : item.isSymbolicLink() ? 'symbolic link' : 'file',
+      }))
+      .sort((a, b) => a.Type < b.Type ? -1 : 0);
   }
   catch {
     throw new Error(ERROR_OPERATION_FAILED);
